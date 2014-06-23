3601c54da1fb317b69c41ec4a8aa4a759c23bcb3
Konstantin Sukharev <poed@me.com>
2014-06-23 15:17:37 +0400

    Create project

---
+++ TLKit CLI/TLKit CLI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TLKit CLI
+//
+//  Created by Konstantin Sukharev on 23.06.14.
+//  Copyright (c) 2014 POed. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
